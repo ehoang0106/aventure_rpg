0198564205f77d0d154b02f4341d856bdffa859c
ehoang0106 <khoahoang1698@gmail.com>
2025-05-28 14:45:45 -0700

    add animation to player

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -7,8 +7,11 @@ var cardinal_direction : Vector2 = Vector2.DOWN
 var direction : Vector2 =  Vector2.ZERO
 
 #state
-
 var state : String = "idle"
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,5 +24,51 @@ func _process(delta: float) -> void:
 	
 	velocity = direction * move_speed
 	
-func _physics_process(delta: float) -> void:
+	if setState() == true || setDirection() == true:
+		updateAnimation()
+	
+	
+	
+	
+func _physics_process(delta):
 	move_and_slide()
+	
+func setDirection() -> bool:
+	var new_direction : Vector2 = cardinal_direction
+	if direction ==  Vector2.ZERO:
+		return false;
+		
+	if direction.y == 0:
+		new_direction = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_direction = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	 
+	if new_direction == cardinal_direction:
+		return false
+		
+	cardinal_direction = new_direction
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
+		
+	return true	
+	
+
+func setState() -> bool:
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	state = new_state
+	return true
+	
+	
+func updateAnimation() -> void:
+	animation_player.play(state + "_" + animDirection())
+	
+
+func animDirection() -> String:  
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
+	
