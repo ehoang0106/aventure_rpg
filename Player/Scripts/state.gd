7ad82f586d81281743e5a517ecbfd2525c523a03
ehoang0106 <khoahoang1698@gmail.com>
2025-05-28 21:11:13 -0700

    state machine

--- Player/Scripts/state.gd
+++ Player/Scripts/state.gd
@@ -1,11 +1,32 @@
-extends Node
+class_name State extends Node
 
+static var player: Player 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+	
+
+# what happens when the player enters this state
+func Enter() -> void:
+	pass
+
+# what happens when the player exits this state
+
+func Exit() -> void:
+	pass
+	
+#what happens during the the _process in this state?
+func Process(_delta : float) -> State:
+	
+	
+	return null
+	
+	
+#what happens during the the _physics_process in this state?
+func Physics(_delta : float) -> State:
+	
+	return null
+	
+func HandleInput(_even : InputEvent) -> State:
+	
+	return null
